0dd7fd5220191ae85625ab1a5f3a229f62a63062
Joao Pedro Rodrigues <joaopedrorodriguescx@gmail.com>
2025-11-10 15:05:32 -0300

    chore-feat: criação de pastas e início da página de startup

---
+++ lib/view/on_boarding/startup_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartupView extends StatefulWidget {
+  const StartupView({super.key});
+
+  @override
+  State<StartupView> createState() => StartupViewState();
+}
+
+class StartupViewState extends State<StartupView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
