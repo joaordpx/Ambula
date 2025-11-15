124db13b26d6b95b20ae02ba6f098bfea03db829
Joao Pedro Rodrigues <joaopedrorodriguescx@gmail.com>
2025-11-15 14:02:14 -0300

    feat: criacao da tela de login seguindo figma

--- frontend/lib/view/on_boarding/startup_view.dart
+++ frontend/lib/view/on_boarding/startup_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/view/login/welcome_view.dart';
 
 class StartupView extends StatefulWidget {
   const StartupView({super.key});
@@ -9,9 +10,23 @@ class StartupView extends StatefulWidget {
 
 class StartupViewState extends State<StartupView> {
   @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const WelcomeView()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Image.asset(
           "assets/imgs/logo.png",
