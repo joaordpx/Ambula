08a2546c266972bae218b93e173679cb80ce3731
TMamede <tamires.m.maia@gmail.com>
2025-12-02 13:42:18 -0300

    feat: seeeder e api

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,19 +2,63 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Localizacao;
+use App\Models\CategoriaProduto;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        User::factory(1)->create();
+        // -----------------------------------------
+        // LOCALIZAÇÕES (Prédio 1, 2 e 3)
+        // -----------------------------------------
+        $locais = [
+            [
+                'nome' => 'Prédio 1 - CCSA',
+                'descricao' => 'Centro de Ciências Sociais Aplicadas.',
+                'latitude' => -16.726300,
+                'longitude' => -43.861900,
+            ],
+            [
+                'nome' => 'Prédio 2 - CCH',
+                'descricao' => 'Centro de Ciências Humanas.',
+                'latitude' => -16.726900,
+                'longitude' => -43.861200,
+            ],
+            [
+                'nome' => 'Prédio 3 - CCET',
+                'descricao' => 'Centro de Ciências Exatas e Tecnológicas.',
+                'latitude' => -16.727400,
+                'longitude' => -43.860800,
+            ],
+        ];
+
+        foreach ($locais as $local) {
+            Localizacao::create($local);
+        }
+
+        // -----------------------------------------
+        // CATEGORIAS DE PRODUTO
+        // -----------------------------------------
+        $categorias = [
+            'Salgados',
+            'Doces',
+            'Bebidas',
+            'Lanches',
+            'Refeições',
+            'Açaí',
+            'Café',
+            'Snacks',
+            'Saudável',
+            'Massas',
+        ];
+
+        foreach ($categorias as $descricao) {
+            CategoriaProduto::create(['descricao' => $descricao]);
+        }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,59 +1,84 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaProdutoController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\ProdutoController; 
-use App\Http\Controllers\LojaController; 
+use App\Http\Controllers\LocalizacaoController;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\LojaController;
 use App\Http\Controllers\PedidoController;
-
+use App\Http\Controllers\SearchController;
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    
+
 
     Route::resource('produtos', ProdutoController::class)->except(['create', 'edit']);
-    
-    
-    Route::resource('lojas', LojaController::class)->except(['create', 'edit', 'index', 'show']);
-    
-    
-    Route::post('pedidos', [PedidoController::class, 'store']);
 
-    Route::get('pedidos', [PedidoController::class, 'index']);
-    
-    Route::get('pedidos/{id}', [PedidoController::class, 'show']);
 
-    
+    Route::post('lojas', [LojaController::class, 'store']);
+    Route::put('lojas/{loja}', [LojaController::class, 'update']);
+    Route::delete('lojas/{loja}', [LojaController::class, 'destroy']);
+
+    Route::post('produtos', [ProdutoController::class, 'store']);
+    Route::put('produtos/{produto}', [ProdutoController::class, 'update']);
+    Route::delete('produtos/{produto}', [ProdutoController::class, 'destroy']);
+
+
+    Route::get('/pedidos', [PedidoController::class, 'index']);
+    Route::get('/pedidos/{id}', [PedidoController::class, 'show']);
+    Route::post('/pedidos', [PedidoController::class, 'store']);
+
+
     Route::put('pedidos/{id}/status', [PedidoController::class, 'updateStatus']);
-    
+
 
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    
+
     Route::get('/me', function (Request $request) {
         return response()->json([
             'user' => $request->user(),
         ]);
     });
-    
-    Route::post('logout', [AuthController::class, 'logout']);
 
+    Route::put('/me', [AuthController::class, 'updateProfile']);
+
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 
-
+// Auth público
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
+// Lojas públicas
+Route::get('lojas', [LojaController::class, 'index']);
+Route::get('lojas/{loja}', [LojaController::class, 'show']);
+
+// Categorias de produto
+Route::get('categorias-produto', [CategoriaProdutoController::class, 'index']);
+
+// Endpoints da Home
+Route::get('home/lojas-populares', [HomeController::class, 'lojasPopulares']);
+Route::get('home/mais-amados', [HomeController::class, 'maisAmados']);
+Route::get('home/disponiveis-agora', [HomeController::class, 'disponiveisAgora']);
+
+Route::get('/localizacoes', [LocalizacaoController::class, 'index']);
+
+
+Route::get('search', [SearchController::class, 'search']);
 
 Route::get('lojas', [LojaController::class, 'index']);
 Route::get('lojas/{loja}', [LojaController::class, 'show']);
 
+Route::get('produtos', [ProdutoController::class, 'index']);
+Route::get('produtos/{produto}', [ProdutoController::class, 'show']);
 
-
+// Debug opcional
 Route::get('/debug-path', function () {
     return base_path();
 });
